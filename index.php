c8bc918b59a5aac6fb3c4492b495666ad170f440
Stéphane ESCOBAR <contact@archimedia.ws>
2017-11-26 18:01:11 +0100

    test WH

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
     }
 
+    
     exit(0);
 }
 
